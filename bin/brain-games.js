886d04ed693a29ed468f2c77ebab92a331abbcfa
Ismailova <Leyla_Ismailova_B@mail.ru>
2024-02-08 11:21:55 +0300

    all save

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,11 @@
 #!/usr/bin/brain-games.js node
+
 console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+
+import userName from "../src/cli.js";
+
+console.log(Hello(`${userName}`));
+
+
+//тут вызов
---
+++ src/cli.js
@@ -0,0 +1,3 @@
+ import _ from 'readline-sync';
+
+export _ ;
