bcba70a6416973f2968e35c155463ac6b4ed140f
LB <Leyla_Ismailova_B@mail.ru>
2024-10-14 17:24:15 +0300

    even-games working without userName

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,25 +1,31 @@
+import greeting from '../src/cli.js';
 import userName from '../src/cli.js';
 import readlineSync from 'readline-sync';
 
+greeting ();
+
 console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
-const getRandomNum = () => Math.floor(Math.random())
+const getRandomNum = () => Math.floor(Math.random()* 101);
 
-for (let i = 0; i<3; i++) {
+for (let i = 0; i<=4; i++) {
     const num = getRandomNum();
     const isEven = (num) => num % 2 === 0;
     const correctAnswer = isEven(num) ? 'yes' : 'no';
-    let userAnswer = readlineSync.question("Question : ${num}?")
     
-    if (userAnswer == correctAnswer){
+    let userAnswer = readlineSync.question(`Question : ${num}?`);
+
+  if (userAnswer == correctAnswer){
     console.log("Correct!");    
     i++;
-  }  
-  
-    else{
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again ${userName}!`);
+    if (i >= 4){
+      console.log(`Congratulations, ${userName} !`);
+    } 
+  }
+   
+  else{
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again ${userName} `);
     break;
-  }  
-  console.log(`Congratulations, ${userName}!`);
+  }
 }
 
