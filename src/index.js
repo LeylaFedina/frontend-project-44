d96428e4719afa7704cfc851fdc158ef4cb82984
LeylaFedina <Leyla_Ismailova_B@mail.ru>
2024-10-21 17:02:39 +0300

    almost woworking

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import readlineSync from 'readline-sync';
 
-const gameRound = (gameQuestion, configureGame)  => { 
+const gameRound = (gameRules, configureGame)  => { 
   const rounds = 3;
 
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
-  console.log(gameQuestion);
+  console.log(gameRules);
 
-for (let i = 0; i<rounds; i++) {
-   const gameKeys = configureGame();
-    let answer = readlineSync.question(`{   }`)
-    if (answer === String(correctAnswer)){
+ //let checkEvenReturn = configureGame();
+ //console.log(checkEvenReturn);
+
+for (let i = 0; i<rounds; i) {
+   const checkEvenReturn = configureGame();
+
+    if (checkEvenReturn.userAnswer === checkEvenReturn.correctAnswer){
         console.log("Correct!");
+        i++;
     }
     else{
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${global.correctAnswer}'. \nLet's try again ${userName}`);
+        console.log(`'${checkEvenReturn.userAnswer}' is wrong answer ;(. 
+        Correct answer was '${checkEvenReturn.correctAnswer}'. 
+        \nLet's try again ${userName}`);
     }
 }
 console.log(`Congratulations, ${userName} !`);
 };
 
 
-export default gameRound();
+export default gameRound;
