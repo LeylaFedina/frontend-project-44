7f90d8f9414d04ec1ceefd80ff19b7767a86b75d
LeylaFedina <Leyla_Ismailova_B@mail.ru>
2024-10-24 12:23:15 +0300

    try 16

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -21,18 +21,18 @@ function calculate (num1, num2, operator) {
       return num1 - num2;
     case ('*'):
       return num1 * num2;
-  };
-};
+  }
+}
 
 function checkCalculation(num1, num2, operator) {
   num1 = getRandomNum();
   num2 = getRandomNum();
   operator = getRandomOperator();
-  let userAnswer = readlineSync.question(`Question: ${num1} ${operator} ${num2}`);
-  let correctAnswer = calculate(num1, num2, operator);    
-    return {
-      userAnswer: userAnswer,
-      correctAnswer: correctAnswer
-    };    
+  const userAnswer = readlineSync.question(`Question: ${num1} ${operator} ${num2}`);
+  const correctAnswer = calculate(num1, num2, operator);
+  return {
+    userAnswer,
+    correctAnswer,
+  };
 }
 export default gameRound(gameRules, checkCalculation);
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node 
-
-import gameRound from '../src/index.js';
 import readlineSync from 'readline-sync';
+import gameRound from '../src/index.js';
+
 
 const gameRules = "Find the greatest common divisor of given numbers.";
 
-const getRandomNum = () => Math.floor(Math.random()* 101);
+const getRandomNum = () => Math.floor(Math.random() * 101);
 
 let numOne = getRandomNum();
 let numTwo = getRandomNum();  
 
 const findCommonDivisor = () => {
-    const minNum = Math.min(numOne, numTwo);
-    for (let i = minNum; i>0; i--){
-        if (numOne % i === 0 && numTwo %i ===0) {
-            return i;
-        }
-    }
+  const minNum = Math.min(numOne, numTwo);
+  for (let i = minNum; i>0; i-1){
+    if (numOne % i === 0 && numTwo %i ===0) {
+      return i;
+    };
+  };
 };
 
 const checkCommonDivision = () => { 
     numOne = getRandomNum();
-    numTwo = getRandomNum();  
+    numTwo = getRandomNum();
     let userAnswer = readlineSync.question(`Question: ${numOne} ${numTwo}`);
     let correctAnswer = String(findCommonDivisor(numOne, numTwo));
 
