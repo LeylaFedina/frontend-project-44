4117f306e6f04631362d3d3911ab82d91255b7e1
LeylaFedina <Leyla_Ismailova_B@mail.ru>
2024-10-28 17:14:01 +0300

    fixed gcd.js bub

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,27 +1,22 @@
 import readlineSync from 'readline-sync';
-
 import gameRound from '../index.js';
-
 import getRandomNum from '../exportCode.js';
 
 const gameRules = 'Find the greatest common divisor of given numbers.';
 
-let numOne = getRandomNum();
-let numTwo = getRandomNum();
-
-const findCommonDivisor = () => {
+const findCommonDivisor = (numOne, numTwo) => {
   const minNum = Math.min(numOne, numTwo);
   for (let i = minNum; i > 0; i -= 1) {
     if (numOne % i === 0 && numTwo % i === 0) {
       return i;
     }
   }
-  return false;
+  return 1;
 };
 
 const checkCommonDivision = () => {
-  numOne = getRandomNum();
-  numTwo = getRandomNum();
+  const numOne = getRandomNum();
+  const numTwo = getRandomNum();
   const userAnswer = readlineSync.question(`Question: ${numOne} ${numTwo}`);
   const correctAnswer = String(findCommonDivisor(numOne, numTwo));
   return {
